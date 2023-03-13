acbc0166c32182d72cc69d936b11fa9c2fb0c001
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-13 20:43:52 +0300

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,9 +4,9 @@ def divisible_by_2(my_list=[]):
     cpy = None
     if (my_list is None):
         cpy = None
-    elif (my_list < 1):
+    elif (len(my_list) < 1):
         cpy = []
-    elif:
+    else:
         cpy = my_list.copy()
         for i in range(len(my_list)):
             cpy[i] = ((my_list[i]%2) == 0)
