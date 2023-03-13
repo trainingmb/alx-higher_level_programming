1e5243dba25d660d4e429a875334f8e9b838955c
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-13 19:03:17 +0300

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,6 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if (idx >= 0) and (idx < len(my_list)):
+    if (my_list is not None) and (idx >= 0) and (idx < len(my_list)):
         my_list[idx] = element
     return(my_list)
-
