cba91d1c539bce534194adbbee52b7285ab97068
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-15 20:35:38 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,6 +5,9 @@ def roman_to_int(roman_string):
     if type(roman_string) != type("") or roman_string == None:
         return 0
     rms = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+    for i in roman_string:
+        if i not in ['M', 'D', 'C', 'L', 'X', 'V', 'I']:
+            return 0
     value = 0
     values = list(map(lambda x: rms[x], roman_string))
     l = len(values)
