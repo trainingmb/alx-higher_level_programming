fc76b44f4edca83743eaa8aa41ab0c4daf5f9b20
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-15 20:11:31 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,9 +5,6 @@ def weight_average(my_list=[]):
     ws = 0.0
     s = 0.0
     for (i,j) in my_list:
-        print(str(i), "     " + str(j))
         ws = ws + i*j
         s = s + j
-    print(ws)
-    print(s)
     return ws/s
