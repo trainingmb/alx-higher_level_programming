c382f06b885d4aab714df6b98943f11e56e91436
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-15 20:24:13 +0300

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
+    rms = []
     for i in a_dictionary.keys():
         if a_dictionary[i] == value:
-            a_dictionary.pop(i, 0)
+            rms.append(i)
+    for i in rms:
+        a_dictionary.pop(i,0)
     return a_dictionary
+
