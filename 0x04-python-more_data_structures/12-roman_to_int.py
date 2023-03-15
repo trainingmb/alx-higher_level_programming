6aae1f1202eb4b9ef07445f369225830c3c13aca
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-15 19:48:54 +0300

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    rms = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+    value = 0
+    values = list(map(lambda x: rms[x], roman_string))
+    l = len(values)
+    i = 0
+    while i < l:
+        if (i+1) < l and values[i] < values[i+1]:
+            value = value - values[i] + values[i+1]
+            i = i + 2
+        else:
+            value = value + values[i]
+            i = i + 1
+    return value
