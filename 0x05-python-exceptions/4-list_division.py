67a8817effc6e3c6fc657f1c3edcf6be8b556c36
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-03-28 04:35:04 +0300

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
-
-def uniq_add(my_list=[]):
-    return sum(set(my_list))
+def list_division(my_list_1, my_list_2, list_length):
+    ret = []
+    index = 0
+    if list_length <= 0:
+        print("out of range")
+        return ret
+    while index < list_length:
+        try:
+            ret.append(my_list_1[index] / my_list_2[index])
+        except TypeError:
+            print("wrong type")
+            ret.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            ret.append(0)
+        except IndexError:
+            print("out of range")
+            ret.append(0)
+        finally:
+            index += 1
+    return ret   
