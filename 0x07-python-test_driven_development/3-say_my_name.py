fed283ec2a7bc74a91d126507ed4bbed4060f82d
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-07 12:58:51 +0300

    Create 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Say my name please
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    prints My name is <first name> <last name>
+
+    Args:
+        first_name (str): the first name
+        last_name (str): the last name
+    """
+    err1 = "first_name must be a string"
+    err2 = "last_name must be a string"
+    say_it = "My name is {} {}"
+    if not isinstance(first_name, str):
+        raise TypeError(err1)
+    if not isinstance(last_name, str):
+        raise TypeError(err2)
+    print(say_it.format(first_name, last_name))
+
+    
