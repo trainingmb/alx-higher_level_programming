1e4eea2d2cf068b8b8e873bc6906c21bdd8e4452
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-07 14:17:23 +0300

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.iters += 1
+    magic_string.iters = getattr(magic_string, 'iters', 0) + 1
     print("BestSchool" + (", BestSchool" * (magic_string.iters-1)))
-magic_string.iters = 0
