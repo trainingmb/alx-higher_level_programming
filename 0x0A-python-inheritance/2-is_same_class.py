793035c625d45c5a7946c47828028335c412a415
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-11 21:30:35 +0300

    Create 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Is same class Module
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Prints a sorted list of ascending order
+    """
+    return type(obj) is a_class
