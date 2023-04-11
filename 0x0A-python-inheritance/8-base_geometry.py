5307e7f3d0c07427dd692c6766ae9865c96c0bdc
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-11 22:43:35 +0300

    Create 8-base_geometry.py

---
+++ 0x0A-python-inheritance/8-base_geometry.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+BaseGeometry Module
+"""
+
+
+class BaseGeometry():
+    """
+    Base Geometry class
+    """
+    def __init__(self, width, height):
+        """
+        BaseGeometry initializer
+        Args:
+            width (int): The width of the object
+            height (int): The height of the object
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        Raises an area exception
+        """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        Integer validator
+        Args:
+            name (str): The name of the parameter to test
+            value (int): The parameter to validate
+        Raises:
+            TypeError: Raised if value is not of type int
+            ValueError: Raised if value is less than or equal to zero
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
