9761b92740eeea29888fb1c1b3edff9e12697599
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-14 22:38:56 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     """
     Read file function
     """
-    if filename != "":
+    if len(filename) < 1:
         with open(filename) as file:
-            print(file.read())
+            print(file.read(), end='')
