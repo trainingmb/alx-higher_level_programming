df483c45ef8ae0e263e2c2b055ccfc9a7563a578
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-15 00:55:31 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,16 +15,17 @@ def main():
     Write a script that adds all arguments
     to a Python list, and then save them to a file
     """
+    filename = "add_item.json"
     with open(filename, 'w') as file:
-            pass
-    my_obj = load_from_json_file("add_item.json")
+        pass
+    my_obj = load_from_json_file(filename)
     arg_list = [sys.argv[i] for i in range(1, len(sys.argv))]
     if not isinstance(my_obj, list):
         save_to_json_file(arg_list)
     else:
         for i in arg_list:
             my_obj.append(i)
-        save_to_json_file(my_obj, "add_item.json")
+        save_to_json_file(my_obj, filename)
 
 
 if __name__ == '__main__':
