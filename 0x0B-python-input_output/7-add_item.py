2c9ab6b7e5750bf50844eae784d60ac461cb261d
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-15 00:25:45 +0300

    Create 7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+7. Load, add, save
+"""
+import json
+import sys
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+def main():
+    """
+    Write a script that adds all arguments
+    to a Python list, and then save them to a file
+    """
+    my_obj = load_from_json_file("add_item.json")
+    arg_list = [sys.argv[i] for i in range(1, len(sys.argv))]
+    if not isinstance(my_obj, list):
+        save_to_json_file(arg_list)
+    else:
+        for i in arg_list:
+            my_obj.append(i)
+        save_to_json_file(my_obj)
+
+
+if __name__ == '__main__':
+    main()
