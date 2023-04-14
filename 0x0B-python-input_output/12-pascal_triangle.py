17e0df6de4a066f175299fc3d09bf84fcfaf48c6
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-15 00:46:46 +0300

    Create 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Pascal Triangle
+    """
+    list_list = [[1]]
+    curr_list = []
+    indx = 0
+    if n <= 0:
+        return (curr_list)
+    elif n == 1:
+        return (list_list)
+    for _ in range(n - 1):
+        for indx in range(1 + len(list_list[-1])):
+            if indx == 0:
+                curr_list.append(1)
+            else:
+                curr_list.append(list_list[-1][indx]
+                    + list_list[-1][indx - 1])
+            if indx == (len(list_list[-1]) - 1):
+                curr_list.append(1)
+                break
+        list_list.append(curr_list.copy())
+        curr_list = []
+    return (list_list)
