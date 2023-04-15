05f2da39cfbb571e346ea24f28e91b8a667dd38d
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-04-15 13:15:05 +0300

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -15,13 +15,13 @@ def print_status():
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
 
-    for l in sys.stdin:
-        line = l.split()
+    for stdlins in sys.stdin:
+        line = stdlins.split()
         try:
             size += int(line[-1])
             code = line[-2]
             status_codes[code] += 1
-        except:
+        except Exception as _:
             continue
         if counter == 9:
             print("File size: {}".format(size))
