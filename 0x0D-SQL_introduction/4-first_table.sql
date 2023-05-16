70e98819babb7604bd730b52988e729e65cdc9c0
trainingmb <availfolly@yahoo.com>
2023-05-16 12:28:33 -0700

    Missing s

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- creates a table called first_table in the current database in your MySQL server
 -- Table creation
-CREATE TABLE IF NOT EXIST first_table(
+CREATE TABLE IF NOT EXISTS first_table(
 	id INT,
 	name VARCHAR(256)
 );
