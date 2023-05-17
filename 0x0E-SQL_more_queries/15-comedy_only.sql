4c3eb6e5572220fd0ace98a1d2638489906773a7
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-18 01:11:28 +0300

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -8,4 +8,5 @@ LEFT JOIN tv_genres ON
 tv_show_genres.genre_id = tv_genres.id
 LEFT JOIN tv_shows ON
 tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.name = "Comedy";
+WHERE tv_genres.name = "Comedy"
+ORDER BY title ASC;
