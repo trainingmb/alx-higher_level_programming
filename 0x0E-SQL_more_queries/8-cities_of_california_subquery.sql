2b0e52789e6768eafbf523ca1b0a385740d11582
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-18 00:21:22 +0300

    Create 8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- Lists all the cities of California that can be found in the database hbtn_0d_usa
+-- Super Query
+SELECT id, name
+FROM cities
+WHERE state_id = 
+-- Subquery
+(SELECT id
+  FROM states
+  WHERE name = "California"
+  LIMIT 1)
+ORDER_BY id;
