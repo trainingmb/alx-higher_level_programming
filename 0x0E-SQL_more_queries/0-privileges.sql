96775186279b4e76d7a2483888902743619e2e3b
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-17 23:32:22 +0300

    Update 0-privileges.sql
    Add user 2 to show list

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
--- Show Privileges
+-- Show Privileges for User 1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
+-- Show Privileges for User 2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
