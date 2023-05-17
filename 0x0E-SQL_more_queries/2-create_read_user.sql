3bb2c7896b4c4c9b51983518233be6d22d1af99d
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-17 23:38:21 +0300

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2
 -- Create the user
-CREATE USER IF NOT EXISTS 'user_0d_2d'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 -- Change Password
 ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Create the database
