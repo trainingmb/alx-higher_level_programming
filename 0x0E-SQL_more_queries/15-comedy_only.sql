b280805d703049d9690550b8df95267442a8d66b
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-18 01:28:40 +0300

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 -- The Select
 SELECT
-	tv_shows.title as title
+	tv_shows.title
 FROM
 tv_show_genres
 LEFT JOIN tv_genres ON
@@ -9,4 +9,4 @@ tv_show_genres.genre_id = tv_genres.id
 LEFT JOIN tv_shows ON
 tv_show_genres.show_id = tv_shows.id
 WHERE tv_genres.name = "Comedy"
-ORDER BY title ASC;
+ORDER BY tv_shows.title ASC;
