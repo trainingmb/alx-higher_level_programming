a1714319a64b249ce8159d306412af19faabb5f9
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-18 01:57:10 +0300

    Create 102-rating_shows.sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,12 @@
+-- Lists all shows from hbtn_0d_tvshows_rate by their rating
+-- Only One Select
+SELECT
+  tv_shows.title,
+  SUM(tv_show_ratings.rate) as rating
+FROM
+  tv_show_ratings
+LEFT JOIN
+  tv_shows ON
+    tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_show_ratings.show_id
+ORDER BY rating DESC;
