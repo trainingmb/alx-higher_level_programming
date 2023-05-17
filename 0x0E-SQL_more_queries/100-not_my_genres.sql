523083dc899a5c3d7be90d0ee2d3dafa8602439f
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-18 01:25:27 +0300

    Create 100-not_my_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,19 @@
+-- List all genres not linked to the show Dexter
+-- Max of 2 Selects
+SELECT
+tv_genres.name as name
+FROM 
+tv_genres
+WHERE
+tv_genres.id NOT IN(
+  SELECT
+    tv_show_genres.genre_id
+  FROM
+    tv_show_genres
+  INNER JOIN
+    tv_shows ON
+    tv_show_genres.show_id = tv_shows.id
+  WHERE
+    tv_shows.title = "Dexter")
+ORDER by name;
+    
