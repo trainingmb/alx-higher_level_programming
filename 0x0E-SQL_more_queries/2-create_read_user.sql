17afdd7c3713c1062e92da8dc49d92c992e24679
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-17 23:37:10 +0300

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,3 +7,5 @@ ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Grant privileges
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2d'@'localhost';
+-- Flush Privileges
+FLUSH PRIVILEGES;
