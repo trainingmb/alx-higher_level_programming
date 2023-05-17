4f686ff1daffd87d1403213f6ee7c7d90aeb346e
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-18 01:08:41 +0300

    Create 15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- The Select
+SELECT
+	tv_shows.title as title
+FROM
+tv_show_genres
+LEFT JOIN tv_genres ON
+tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_shows ON
+tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.name = "Comedy";
