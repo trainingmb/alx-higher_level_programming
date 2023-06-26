0c0d25321fcef137a46777c905e03a912c7477f7
trainingmb <availfolly@yahoo.com>
2023-06-26 01:23:31 -0700

    Ocurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  list.forEach(myFunction);
+
+  function myFunction (value, index, array) {
+    if (value == searchElement) {
+	    count += 1;
+    }
+  }
+  return count;
+};
