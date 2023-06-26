cc8f06d859be3e9e131087be29b7663eea542350
trainingmb <availfolly@yahoo.com>
2023-06-26 01:41:21 -0700

    Reverserunion

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-exports.esrever = function (list){
-    let x = list.length - 1;
-    let reved = [];
-    while (x >= 0) {
-        reved.push(list[x]);
-        x -= 1;
-    }
-    return reved; 
+exports.esrever = function (list) {
+  let x = list.length - 1;
+  const reved = [];
+  while (x >= 0) {
+    reved.push(list[x]);
+    x -= 1;
+  }
+  return reved;
 };
