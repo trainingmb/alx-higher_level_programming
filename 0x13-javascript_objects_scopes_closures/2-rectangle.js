49d40e1764678309841bf536b7ad9a20c042441a
trainingmb <availfolly@yahoo.com>
2023-06-25 23:53:03 -0700

    Working

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,8 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
-      this.height = h;
       this.width = w;
+      this.height = h;
     }
   }
 };
