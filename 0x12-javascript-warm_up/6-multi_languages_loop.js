7bc71cdd92dc31bec227c0226ecd8c909efa5235
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-11-13 23:50:41 +0300

    Add files

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let i = 0;
+
+const loopArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+while (i < loopArray.length) {
+  console.log(loopArray[i]);
+  i++;
+}
