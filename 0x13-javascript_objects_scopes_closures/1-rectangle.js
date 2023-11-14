af168d753aec167bd5dde7452af5f5d2532a2f13
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-11-14 17:59:11 +0000

    Reactangle number 2

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Make an empty Rectangle class
+
+module.exports = class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
--- 0x13-javascript_objects_scopes_closures/README.md
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -1,2 +1,3 @@
 0x13. JavaScript - Objects, Scopes and Closures
 0. Rectangle #0
+1. Rectangle #1
