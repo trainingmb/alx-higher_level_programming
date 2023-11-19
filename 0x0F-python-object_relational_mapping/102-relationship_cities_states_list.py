5bba7d6ac2443981562718e7c57f148c4d96293e
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-11-19 15:18:46 -0800

    Added 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -3,7 +3,9 @@
 List all states using SQLAlchemy
 """
 import sys
-from relationship_state import Base, State, City
+
+from relationship_state import Base, State
+from relationship_city import City
 
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
