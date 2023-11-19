106a2bb117b86f7a8d1720041a518fcd6f2de49f
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-11-19 13:12:01 -0800

    Added 100-relationship_states_cities.py

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+List all states using SQLAlchemy
+"""
+import sys
+from relationship_state import Base, State, City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def engine_session_creator(gurl):
+    """
+    Create the Engine and Session to be used
+    """
+    engine = create_engine(gurl,
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    return engine, session
+
+
+
+if __name__ == "__main__":
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                sys.argv[2], sys.argv[3])
+    engine, Session = engine_session_creator(url)
+    session  = Session()
+    new_city = City(name="San Francisco", state=State(name="California"))
+    session.add(new_city)
+    session.commit()
+    session.commit()
+
