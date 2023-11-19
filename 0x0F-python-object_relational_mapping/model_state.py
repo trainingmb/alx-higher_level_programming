20ad2df22aa6a8ea93ff142447f617ad6c7fc7dc
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-11-19 11:14:05 -0800

    Include the City classx

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """
 Models using SQLAlchemmy
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -16,4 +16,15 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True,
             nullable=False)
     name = Column(String(128), nullable=False)
+
+
+class City(Base):
+    """
+    Model for city class
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+            nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
 
