8f731a56d0aaeb648a953f8c1432486a0b1b3b28
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-11-19 10:37:35 -0800

    Added 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Update a state
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def engine_session_creator(gurl):
+    """
+    Create the Engine and Session to be used
+    """
+    engine = create_engine(gurl,
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    return engine, session
+
+
+
+if __name__ == "__main__":
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                sys.argv[2], sys.argv[3])
+    engine, Session = engine_session_creator(url)
+    session  = Session()
+    upstate = session.query(State).filter(State.id == 2).first()
+    upstate.name = "New Mexico"
+    session.commit()
+
