d90fc6523e569a20bd0a80b934e66c21410182f8
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-12-01 03:30:16 -0800

    Added 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     from urllib.request import urlopen
     with urlopen(url) as response:
         the_page = response.read()
-        print("Body Response:")
+        print("Body response:")
         print("\t- type: {}".format(type(the_page)))
         print("\t- content: {}".format(the_page))
         print("\t- utf8 content: {}".format(the_page.decode('utf8')))
