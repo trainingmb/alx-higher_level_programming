c3e81f38308687b96f5858278914ccdb5ec6ccc0
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-12-01 02:22:27 -0800

    Added 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+POST an email #0
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv, exit
+    if len(argv) < 3:
+        exit()
+    url = str(argv[1])
+    values = {'email' : str(argv[2])}
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    data = urlencode(values).encode('ascii')
+    req = Request(url, data)
+    with urlopen(req) as response:
+        print(response.read())
