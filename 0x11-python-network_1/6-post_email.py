47eab32f073fef45c04708ef94a41a870d57fe98
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-12-01 03:34:52 -0800

    Added 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     from sys import argv
     import requests
     url = str(argv[1])
-    response = requests.put(url, data={'email': argv[2]})
+    response = requests.post(url, data={'email': argv[2]})
     print(response.text)
