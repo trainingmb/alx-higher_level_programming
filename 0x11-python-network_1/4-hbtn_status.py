34a4f0ec84bb47fc862bc961140c8f8e08e5efc0
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-12-01 03:29:25 -0800

    Added 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     import requests
     response = requests.get(url)
-    print("Body Response:")
+    print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
