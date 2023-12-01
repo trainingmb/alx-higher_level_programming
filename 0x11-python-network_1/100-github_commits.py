7dc03780ce7ef40c23f207c1fdd7544fda756066
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-12-01 11:51:39 -0800

    Added 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Interview Question Github API
+"""
+
+
+def printCommits(jsn, limit=10):
+    """
+    Print <limit> commit message sha and commiter
+    name
+    """
+    i = 0
+    if len(jsn) < limit:
+        limit = len(jsn)
+    while i < limit:
+        sha = jsn[i].get('sha')
+        name = jsn[i].get('commit').get('author').get('name')
+        print("{}: {}".format(sha, name))
+        i += 1
+
+
+if __name__ == "__main__":
+    from sys import argv, exit
+    import requests
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
+    header = {'Accept': 'application/vnd.github+json',
+              'X-GitHub-Api-Version': '2022-11-28'}
+    data = {'per_page': 10}
+    response = requests.get(url, headers=header, data=data)
+    if response.status_code == 200 and \
+       'application/json' in response.headers.get('Content-Type'):
+        jsn = response.json()
+        printCommits(jsn)
