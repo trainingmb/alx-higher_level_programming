4ecb18c22912b0fdefb33475c94fcb2c2f4bcfd3
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-12-01 03:11:25 -0800

    Added 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Error code #0
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv, exit
+    if len(argv) < 2:
+        exit()
+    url = str(argv[1])
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    from urllib.error import HTTPError
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except HTTPError as e:
+        print('Error code: ', e.code)
