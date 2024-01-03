2b5eecc80a595a07e12956555c15a9d75a50deb5
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-03 01:58:36 -0800

    Added 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
 
-//For handling arguments and files
+// For handling arguments and files
 const process = require('process');
 const fs = require('fs');
 
-
-//Arguments variable
+// Arguments variable
 const args = process.argv;
 
-//Argument provided
-if (args[2] != null){
-	fs.readFile(args[2], {encoding: 'utf-8'}, function(err, data) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-		console.log(data);
-	});
+// Argument provided
+if (args[2] != null) {
+  fs.readFile(args[2], { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
 }
