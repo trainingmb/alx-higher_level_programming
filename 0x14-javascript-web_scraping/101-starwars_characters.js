6585e1955710ecf71f35e8fc6b5d10c063264349
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-03 21:41:21 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,9 +4,11 @@
 const process = require('process');
 const request = require('request');
 
-// Arguments variable
+// Variables
 const args = process.argv;
 const hit = {};
+var counterstop = 0;
+var counterx = 0;
 
 // Iterate through info
 function allInfo (info) {
@@ -26,14 +28,15 @@ function getPeople (purl) {
   });
 }
 
-counterstop = 0;
-counterx = 0;
-
+//Counter for when to log
 function counter () {
   counterx++;
   if (counterx === counterstop) {
     for (const [key, value] of Object.entries(hit)) {
       console.log(value);
+      if (key === '1') {
+        counterx++;
+      }
     }
   }
 }
@@ -41,7 +44,7 @@ function counter () {
 // Argument provided
 if (args[2] != null) {
   const url = 'https://swapi-api.alx-tools.com/api/films/';
-  const r = request(url + args[2], function (err, response, body) {
+  request(url + args[2], function (err, response, body) {
     if (!err && response.statusCode === 200) {
       allInfo(JSON.parse(body));
       counterstop = JSON.parse(body).characters.length;
