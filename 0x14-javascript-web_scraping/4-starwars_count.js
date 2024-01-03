910b3ec891bdc92e3c2d2e777e4cf25b7bc3eb36
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-03 22:02:19 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,14 +7,21 @@ const request = require('request');
 // Arguments variable
 const args = process.argv;
 
-// Argument provided
 if (args[2] != null) {
+  const wa = 'https://swapi-api.alx-tools.com/api/people/18/';
   const id = '18';
   var url = 'https://swapi-api.alx-tools.com/api/people/' + id;
   let count = 0;
+  request(args[2], function (err, response, body) {
   request(url, function (err, response, body) {
     if (!err && response.statusCode === 200) {
       const info = JSON.parse(body);
+      for (let i = 0; i < info.results.length; i++) {
+        if (info.results[i].characters.includes(wa)) {
+          count++;
+        }
+      }
+      console.log(count);
       console.log(info.films.length);
     }
   });
