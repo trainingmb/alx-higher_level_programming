8bb9d7f9230c402dcc43e5680081708cc193075b
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-11 01:47:58 +0300

    Update 2-script.js

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,7 +1,6 @@
-//Change the color of the header text on clicking DIV tag
-$(document).ready(function(){
-  $('div#red_header').click(function(){
+//Change the color of the header text on clicking DIV tag 
+$(document).ready(function () {
+  $('div#red_header').click(function () {
     $('header').css('color', '#FF0800');
   });
 });
-
