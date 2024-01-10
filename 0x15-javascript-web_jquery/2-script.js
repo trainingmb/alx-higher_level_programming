e36d7cfbf95312c38b378b63afc78504785fa538
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-01-10 14:46:16 -0800

    Added 2-script.js

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,7 @@
+//Change the color of the header text on clicking DIV tag
+$(document).ready(function(){
+  $('div#red_header').click(function(){
+    $('header').css('color', '#FF0800');
+  });
+});
+
